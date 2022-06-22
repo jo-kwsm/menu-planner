bd497e0d89dcfec305aab2a89fd9be94cd57898b
jo-kwsm <54238261+jo-kwsm@users.noreply.github.com>
2022-06-23 00:46:37 +0900

    カテゴリと負担度合いの表示 (#5)

--- backend/app/domain/menu.go
+++ backend/app/domain/menu.go
@@ -3,7 +3,8 @@ package domain
 type Menu struct {
 	MenuId     int64    `json:"menuId" form:"menuId"`
 	MenuName   string   `json:"menuName" form:"menuName"`
-	Calorie    string   `json:"calorie" form:"calorie"`
+	Calorie    int64    `json:"calorie" form:"calorie"`
 	BurdenId   int64    `json:"burdenId" form:"burdenId"`
-	MenuRecipe []Recipe `json:"recipe" form:recipe`
+	BurdenName string   `json:"burdenName" form:"burdenName"`
+	MenuRecipe []Recipe `json:"recipe" form:"recipe"`
 }
