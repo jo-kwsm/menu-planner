6c4b9e00f0a71a91cff6c496aec201519da9078f
jo-kwsm <54238261+jo-kwsm@users.noreply.github.com>
2022-07-08 00:14:25 +0900

    add menu list page (#8)
    * add menu list page
    
    * add line break

---
+++ frontend/app/src/components/MenuList.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useContext } from 'react'
+import { client } from '../libs/axios'
+import { MenuContext } from '../provider/MenuProvider'
+
+const omitText = (text: string): string => {
+  if (text.length > 10) {
+    return text.substring(0, 7) + '...'
+  }
+  return text
+}
+
+export const MenuList = () => {
+  const { menus, setMenus } = useContext(MenuContext)
+
+  useEffect(() => {
+    client.get('menu').then(({ data }) => {
+      setMenus(data)
+    })
+    console.log(menus)
+  }, [])
+
+  return (
+    <div className="p-4 border border-gray-200 rounded shadow-lg">
+      <p className="font-bold mb-2">レシピ一覧</p>
+      <table className="border-collapse table-auto">
+        <thead>
+          <tr>
+            <th className="py-1">番号</th>
+            <th className="p-1">料理名</th>
+            <th className="p-1">負担</th>
+          </tr>
+        </thead>
+        <tbody>
+          {menus.map((menu) => {
+            return (
+              <tr key={menu.menuId}>
+                <td className="p-1">{menu.menuId}</td>
+                <td className="p-1">{omitText(menu.menuName)}</td>
+                <td className="p-1">{menu.burdenName}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
---
+++ frontend/app/src/types/index.ts
@@ -0,0 +1,8 @@
+export interface MenuType {
+  menuId: number
+  menuName: string
+  calorie: number
+  burdenId: number
+  burdenName: string
+  // recipe:
+}
