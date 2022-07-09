2645299555224da1c8d2ae28bca62fece1e138c5
jo-kwsm <54238261+jo-kwsm@users.noreply.github.com>
2022-07-09 20:09:06 +0900

    Bootstrapの導入 (#12)
    * add bootstrap
    
    * delete css

--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Header = () => {
     return (
-        <header>
-            <Link to={`/random`}>一日分の献立</Link>
-            <Link to={`/list`}>レシピ一覧</Link>
-        </header>
+        <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+                <Nav.Link href="/random">一日分の献立</Nav.Link>
+                <Nav.Link href="/list">レシピ一覧</Nav.Link>
+            </Nav>
+        </Navbar>
     )
 }
