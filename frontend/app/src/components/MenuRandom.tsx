2f9c10dfda1ad6e3be3bb9c4862fa3ce979beea4
jo-kwsm <ytk.kwsm@gmail.com>
2022-07-09 13:15:57 +0900

    prepare a display

--- frontend/app/src/components/MenuRandom.tsx
+++ frontend/app/src/components/MenuRandom.tsx
@@ -25,31 +25,25 @@ export const MenuRandom = () => {
       <table className="border-collapse table-auto">
         <thead>
           <tr>
-            <th className="py-1">番号</th>
+            <th className="py-1"></th>
             <th className="p-1">料理名</th>
             <th className="p-1">負担</th>
           </tr>
         </thead>
-        <tbody>
-          {plans.map((plan, index) => {
-            return (
-              <tr key={index}>
-                <tbody>
-                  <tr>
-                    <td className="p-1">昼</td>
-                    <td className="p-1">{omitText(plan.lunch.menuName)}</td>
-                    <td className="p-1">{plan.lunch.burdenName}</td>
-                  </tr>
-                  <tr>
-                    <td className="p-1">夜</td>
-                    <td className="p-1">{omitText(plan.dinner.menuName)}</td>
-                    <td className="p-1">{plan.dinner.burdenName}</td>
-                  </tr>
-                </tbody>
-              </tr>
-            )
-          })}
-        </tbody>
+        {plans[0] &&
+          <tbody>
+            <tr>
+              <td className="p-1">昼</td>
+              <td className="p-1">{omitText(plans[0].lunch.menuName)}</td>
+              <td className="p-1">{plans[0].lunch.burdenName}</td>
+            </tr>
+            <tr>
+              <td className="p-1">夜</td>
+              <td className="p-1">{omitText(plans[0].dinner.menuName)}</td>
+              <td className="p-1">{plans[0].dinner.burdenName}</td>
+            </tr>
+          </tbody>
+        }
       </table>
     </div>
   )
